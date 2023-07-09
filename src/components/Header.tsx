1b68a2cf81f0dc56a96314bf3c878c7a1c37d50a
ArthurAlexi <aammenezes@sga.pucminas.br>
2023-07-09 17:53:09 -0300

    feat: update Header and Global Layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import Image from "next/image";
 import {signIn, useSession, signOut} from 'next-auth/react'
 import {AiOutlineMenu} from 'react-icons/ai'
+import Link from "next/link";
 const Header = ()=>{
     
     const {status, data} = useSession()
@@ -13,9 +14,11 @@ const Header = ()=>{
     const handleLogoutClick = ()=> {signOut(); handleMenuClick()}
    return (
     <div className="container mx-auto p-5 py-0 h-[93px] flex justify-between items-center">
-        <div className="relative h-[32px] w-[182px]">
-          <Image src="/logo.png" alt="Full Stack Week" fill />
-        </div>
+        <Link href='/'>
+            <div className="relative h-[32px] w-[182px]">
+                <Image src="/logo.png" alt="Full Stack Week" fill />
+            </div>
+        </Link>
         {status === 'unauthenticated' && (
             <button className="text-primary text-sm font-semibold" onClick={handleLoginClick}>Login</button>
         )}
@@ -26,7 +29,7 @@ const Header = ()=>{
                 className="rounded-full shadow-md"/>
                 {menuIsOpen && (
                     <div 
-                    className="absolute top-14 left-0 w-full h-full bg-white rounded-lg shadow-md flex flex-col justify-center items-center">
+                    className="z-50 absolute top-14 left-0 w-full h-full bg-white rounded-lg shadow-md flex flex-col justify-center items-center">
                         <button className="text-primary text-sm font-semibold" onClick={handleMenuClick}>logout</button>
                     </div>
                 )}
