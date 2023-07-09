62e3d193830cc1829b2e00c3694a249622a56d87
ArthurAlexi <aammenezes@sga.pucminas.br>
2023-07-09 16:54:25 -0300

    feat: add Footer component

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,7 @@ module.exports = {
         secondary : '#312A4F',
         grayPrimary : '#717171',
         grayLighter : '#BBBFBF',
+        waterWhite : "#F5F5F5"
       },
       textColor : {
         dark : '#717171'
