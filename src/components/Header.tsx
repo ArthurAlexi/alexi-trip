15a08849ce2dee770c1e6cbcae9f7a52de084a4a
Arthur Alexi <arthuralexi@hotmail.com>
2023-08-12 17:06:14 -0300

    feat: add button 'my Trips' inn Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,12 +6,13 @@ import {AiOutlineMenu} from 'react-icons/ai'
 import Link from "next/link";
 const Header = ()=>{
     
-    const {status, data} = useSession()
+    const {status, data} = useSession() 
     const [menuIsOpen, setMenuIsOpen] = useState(false)
 
     const handleMenuClick = ()=> setMenuIsOpen(!menuIsOpen)
     const handleLoginClick = () => signIn()
     const handleLogoutClick = ()=> {signOut(); handleMenuClick()}
+    
    return (
     <div className="container mx-auto p-5 py-0 h-[93px] flex justify-between items-center">
         <Link href='/'>
@@ -29,8 +30,13 @@ const Header = ()=>{
                 className="rounded-full shadow-md"/>
                 {menuIsOpen && (
                     <div 
-                    className="z-50 absolute top-14 left-0 w-full h-full bg-white rounded-lg shadow-md flex flex-col justify-center items-center">
-                        <button className="text-primary text-sm font-semibold" onClick={handleMenuClick}>logout</button>
+                    className="z-50 absolute top-14 left-0 w-full h-[100px] bg-white rounded-lg shadow-md flex flex-col justify-center items-center">
+                        <Link href='/my-trips'>
+                        <button  className="text-primary text-sm font-semibold pb-2 border-b border-solid border-grayLighter" >
+                            My trips
+                        </button>
+                        </Link>
+                        <button className="text-primary text-sm font-semibold pt-2" onClick={handleLogoutClick}>logout</button>
                     </div>
                 )}
             </div>
