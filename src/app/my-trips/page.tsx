0bd4b561d13f837ed48821f8eee3e3d9c0734b29
Arthur Alexi <arthuralexi@hotmail.com>
2023-08-13 11:42:55 -0300

    feat: add my trips pages and the cancel trips

--- src/app/my-trips/page.tsx
+++ src/app/my-trips/page.tsx
@@ -1,32 +1,52 @@
 'use client'
 
-import { TripReservation } from '@prisma/client';
+import { Prisma, TripReservation } from '@prisma/client';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
-import React, {useEffect,useState} from 'react'
+import React, { useEffect, useState } from 'react'
+import UserReservationItem from './components/UserReservationItem';
+import Button from '@/components/Button';
+import Link from 'next/link';
 
-const MyTrips =  ()=>{
+const MyTrips = () => {
 
     const router = useRouter()
-    const {status, data} = useSession()
-    const [reservations, setReservations] = useState<TripReservation[]>([])
+    const { status, data } = useSession()
+    const [reservations, setReservations] = useState<Prisma.TripReservationGetPayload<{
+        include: { trip: true }
+    }>[]>([])
 
-    useEffect(()=>{
-        if(status === "authenticated")
+    const fetchReservations = async () => {
+        const response = await fetch(`http://localhost:3000/api/user/${(data?.user as any).id}/trips`)
+        const json = await response.json()
+        setReservations(json)
+    }
+
+    useEffect(() => {
+        if (status === "authenticated")
             router.push('/')
 
-        const fetchReservations =async () => {
-            const response = await fetch(`http://localhost:3000/api/user/${(data?.user as any).id}/trips`)
-            const json = await response.json()
-            setReservations(json)
-        }
+        
         fetchReservations()
 
 
     }, [status])
 
-    return(
-        <div className='container mg'>
+    return (
+        <div className='container mx-auto p-5'>
+            <h1 className="font-semibold text-dark text-xl">My trips</h1>
+            {
+                reservations.length > 0 ? (
+                    reservations?.map((reservation) => 
+                    <UserReservationItem key={reservation.id} reservation={reservation} fetchReservations={fetchReservations}/>)
+                ) 
+                    : 
+                <div className="flex flex-col">
+                    <p className='font-medium text-dark mt-2'>you have no trips booked</p>
+                    <Link href="/"><Button className='w-full mt-2'>Makereservation</Button></Link>
+                </div>
+
+            }
         </div>
     )
 }
